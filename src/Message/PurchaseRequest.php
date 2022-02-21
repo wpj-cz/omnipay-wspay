633dc1f5a1d223546ab46ee6d7b624312a76946a
Ondřej Beneš <ondrab97@gmail.com>
2022-02-21 08:51:37 +0100

    add Version to request data

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -8,6 +8,8 @@ class PurchaseRequest extends AbstractRequest
     public function getData()
     {
         $data = array();
+        
+        $data['Version'] = $this->getVersion();
         $data['ShopID'] = $this->getShopId();
         $data['ShoppingCartID'] = $this->getShoppingCartId();
         $data['TotalAmount'] = $this->getTotalAmount();
@@ -23,6 +25,7 @@ class PurchaseRequest extends AbstractRequest
         $data['CustomerEmail'] = $this->getCustomerEmail();
         $data['CustomerPhone'] = $this->getCustomerPhone();
         $data['CustomerCountry'] = $this->getCustomerCountry();
+        
         return $data;
     }
 
